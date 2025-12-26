995437483d38dc950251d888de6c02e3acce175f
ethanturk <ethan.turk@gmail.com>
2025-12-26 13:08:20 -0600

    Fix formatting error

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,7 +4,7 @@ export default function NavBar({ onShowSearch, onShowDocuments, onShowSummarize 
     return (
         <AppBar position="static">
             <Toolbar>
-                <Typography variant="h6" component="div" className={['flex-grow-1', 'mb-4'].join(' ')}>
+                <Typography variant="h6" component="div" className="flex-grow-1">
                     Agent Document Manager
                 </Typography>
                 <Button color="inherit" onClick={onShowSearch}>Search</Button>
