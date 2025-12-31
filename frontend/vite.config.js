8880dbfaa9fd3c783f121cbd8ae507b4e60225b0
Ethan Turk <ethan.turk@gmail.com>
2025-12-31 10:09:54 -0600

    Allow all hosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,6 +12,6 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
-    allowedHosts: ['localhost', '127.0.0.1', '0.0.0.0', '*.home.ethanturk.com']
+    allowedHosts: ['true']
   }
 })
