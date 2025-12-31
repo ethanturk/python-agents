e30a2b71c9d50929bb87c006b0dcbc4c95e59333
Ethan Turk <ethan.turk@gmail.com>
2025-12-31 13:05:16 -0600

    Tweak vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.VITE_APP_BASE || '/',
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.js',
-  },
-  server: {
-    host: true,
-    port: 3000,
-    allowedHosts: ['.ethanturk.com']
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    base: env.VITE_APP_BASE || '/',
+    plugins: [react()],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/setupTests.js',
+    },
+    server: {
+      host: true,
+      port: 3000,
+      allowedHosts: ['.ethanturk.com']
+    }
   }
 })
