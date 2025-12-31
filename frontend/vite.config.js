3fbb55a1ee47c9254ca763d0e3cb95ff0303edc5
Ethan Turk <ethan.turk@gmail.com>
2025-12-31 13:06:50 -0600

    Add debug info

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
+  console.log("DEBUG: VITE_APP_BASE:", env.VITE_APP_BASE)
+  console.log("DEBUG: Mode:", mode)
   return {
     base: env.VITE_APP_BASE || '/',
     plugins: [react()],
